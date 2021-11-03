0f9299f5b455bfc4199d5cd854fa41cf64f134a6
SvitlanaDmytriv <79254749+SvitlanaDmytriv@users.noreply.github.com>
2021-11-03 14:34:14 +0200

    Initial commit

---
+++ src/component/ContactForm/ContactForm.js
@@ -0,0 +1,146 @@
+import { useState } from 'react';
+import shortid from 'shortid';
+import s from './ContactForm.module.css';
+
+function ContactForm({ addContact }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  console.log(name, number);
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    addContact({ name, number });
+    setName('');
+    setNumber('');
+  };
+
+  const nameInputId = shortid.generate();
+  const phoneInputId = shortid.generate();
+
+  return (
+    <form className={s.form} onSubmit={handleSubmit}>
+      <label className={s.label} htmlFor={nameInputId}>
+        Name
+      </label>
+      <input
+        className={s.input}
+        type="text"
+        name="name"
+        value={name}
+        id={nameInputId}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+        required
+        onChange={handleChange}
+      />
+
+      <label className={s.label} htmlFor={phoneInputId}>
+        Number
+      </label>
+      <input
+        className={s.input}
+        type="tel"
+        name="number"
+        value={number}
+        id={phoneInputId}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+        required
+        onChange={handleChange}
+      />
+
+      <button className={s.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
+
+// class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   nameInputId = shortid.generate();
+//   phoneInputId = shortid.generate();
+
+//   handleChange = e => {
+//     const { name, value } = e.target;
+
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.addContact({ ...this.state });
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({
+//       name: '',
+//       number: '',
+//     });
+//   };
+//   render() {
+//     const { nameInputId, phoneInputId, handleChange, handleSubmit } = this;
+//     const { name, number } = this.state;
+//     return (
+//       <form className={s.form} onSubmit={handleSubmit}>
+//         <label className={s.label} htmlFor={nameInputId}>
+//           Name
+//         </label>
+//         <input
+//           className={s.input}
+//           type="text"
+//           name="name"
+//           value={name}
+//           id={nameInputId}
+//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+//           required
+//           onChange={handleChange}
+//         />
+
+//         <label className={s.label} htmlFor={phoneInputId}>
+//           Number
+//         </label>
+//         <input
+//           className={s.input}
+//           type="tel"
+//           name="number"
+//           value={number}
+//           id={phoneInputId}
+//           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//           title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+//           required
+//           onChange={handleChange}
+//         />
+
+//         <button className={s.btn} type="submit">
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+
+export default ContactForm;
