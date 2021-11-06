064a7eea33e0ccc0b02488893d9c73ac436c11a7
SvitlanaDmytriv <svetagawrylyuk26@gmail.com>
2021-11-06 18:20:42 +0200

    done

--- src/component/ContactList/ContactList.js
+++ src/component/ContactList/ContactList.js
@@ -1,3 +1,4 @@
+// Redux Toolkit
 import { PersonXFill } from 'react-bootstrap-icons';
 import PropTypes from 'prop-types';
 import s from './ContactList.module.css';
@@ -27,8 +28,10 @@ function ContactList() {
     <ul className={s.list}>
       {visibleContacts.map(({ name, number, id }) => (
         <li key={id} className={s.listItem}>
-          <span className={s.contact}>{name} :</span>
-          <span className={s.contact}>{number}</span>
+          <div className={s.contactItem}>
+            <span className={s.contact}>{name} :</span>
+            <span className={s.contact}>{number}</span>
+          </div>
           <Button
             className={s.button}
             type="button"
@@ -43,8 +46,56 @@ function ContactList() {
 }
 
 ContactList.propTypes = {
-  contactsList: PropTypes.array,
-  deleteContact: PropTypes.func,
+  contactsAll: PropTypes.array,
+  value: PropTypes.string,
+  deleteItem: PropTypes.func,
 };
 
 export default ContactList;
+
+//Redux
+
+// import { connect } from "react-redux";
+// import s from "./ContactList.module.css";
+// import {deleteContact} from "../../redux/contactsActions";
+
+// function ContactList({ contactsAll, deleteItem }) {
+//
+//   return (
+// <ul className={s.list}>
+//   {contactsAll.map(({ name, number, id }) => (
+//     <li key={id} className={s.listItem}>
+//       <span className={s.contact}>{name} :</span>
+//       <span className={s.contact}>{number}</span>
+//       <Button
+//         className={s.button}
+//         type="button"
+//         onClick={() => deleteItem(id)}
+//       >
+//         <PersonXFill width="20" height="30" className={s.icon} />
+//       </Button>
+//     </li>
+//   ))}
+// </ul>
+//   );
+// }
+
+// function visibleContacts(contactsAll, value) {
+//   const normalizedFilter = value.toLocaleLowerCase();
+//   return contactsAll.filter((contact) =>
+//     contact.name.toLocaleLowerCase().includes(normalizedFilter)
+//   );
+// }
+
+// const mapStateToProps = ({ contacts: { items, filter } }) => {
+//   console.log(filter);
+//   return {
+//     contactsAll: visibleContacts(items, filter),
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => ({
+//   deleteContact: (id) => dispatch(deleteContact(id)),
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
--- src/component/ContactsSection/ContactsSection.js
+++ src/component/ContactsSection/ContactsSection.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import s from './ContactsSection.module.css';
 import ContactFilter from '../ContactFilter/ContactFilter';
 import ContactList from '../ContactList/ContactList';
@@ -8,7 +9,6 @@ import { useSelector } from 'react-redux';
 export default function ContactsSection({ toggleModall }) {
   const contactsAll = useSelector(state => state.contacts.items);
   const filterValue = useSelector(state => state.contacts.filter);
-  console.log(contactsAll);
 
   const filterContacts = () => {
     const normalizedFilter = filterValue.toLowerCase();
@@ -25,7 +25,7 @@ export default function ContactsSection({ toggleModall }) {
         <PlusCircleFill width="40" height="40" className={s.icon} />
       </Button>
       {contactsAll.length > 1 && <ContactFilter />}
-      {visibleContacts.length > 0 ? (
+      {contactsAll.length > 0 ? (
         <ContactList />
       ) : (
         <p className={s.text}>
@@ -41,3 +41,59 @@ export default function ContactsSection({ toggleModall }) {
     </div>
   );
 }
+
+ContactList.propTypes = {
+  contactsAll: PropTypes.array,
+  filterValue: PropTypes.string,
+  toggleModall: PropTypes.func,
+};
+
+// Redux
+
+// import { connect } from "react-redux";
+// import s from "./ContactList.module.css";
+
+// function ContactsSection({ visibleContacts,contactsAll,filterValue,toggleModall }) {
+//
+//   return (
+//     <div className={s.container}>
+//       <h2 className={s.title}>Contacts</h2>
+//       <Button className={s.button} type="button" onClick={toggleModall}>
+//         <PlusCircleFill width="40" height="40" className={s.icon} />
+//       </Button>
+//       {contactsAll.length > 1 && <ContactFilter />}
+//       {contactsAll.length > 0 ? (
+//         <ContactList />
+//       ) : (
+//         <p className={s.text}>
+//           Contact list is empty. Add your first contact to the list.
+//         </p>
+//       )}
+
+//       {visibleContacts < 1 && filterValue !== '' && (
+//         <p className={s.text}>
+//           There is no contact with the name "{filterValue}" in the list
+//         </p>
+//       )}
+//     </div>
+//   );
+// }
+
+//  const visibleContacts = (contactsAll,filterValue) => {
+//     const normalizedFilter = filterValue.toLowerCase();
+
+//     return contactsAll.filter(contact =>
+//       contact.name.toLowerCase().includes(normalizedFilter),
+//     );
+//   };
+//
+
+// const mapStateToProps = ({ contacts: { items, filter } }) => {
+//     return {
+//     visibleContacts: visibleContacts(items, filter),
+//     contactsAll:items,
+//     filterValue:filter,
+//   };
+// };
+
+// export default connect(mapStateToProps, _)(ContactsSection);
